808fc2857c7f9e79f4633f956e7799eecdd9fe9a
Amehra2 <akashmehra2001@hotmail.com>
2021-12-23 15:33:10 +0000

    1 file modified

--- Database.js
+++ Database.js
@@ -1,17 +1,17 @@
 var sqlite3 = require('sqlite3').verbose()
 
 // open the database
-let db = new sqlite3.Database('patients.db', (err) => {
+let db = new sqlite3.Database('patient.db', (err) => {
     if (err) {
       console.error(err.message);
       throw err
     }
-    console.log('Connected to the patients database.');
+    console.log('Connected to the patient database.');
   });
 
 
   // create table 'book'
-  const sql='CREATE TABLE patients(name text)';
+  const sql='CREATE TABLE patient(name text)';
   db.run(sql, (err) => {
     if (err) {
         // Table already created
