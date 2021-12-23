6510a02c66a8d35e281cff3ac39978af0277eaf0
Amehra2 <akashmehra2001@hotmail.com>
2021-12-23 15:25:50 +0000

    1 file modified

--- Database.js
+++ Database.js
@@ -1,17 +1,17 @@
 var sqlite3 = require('sqlite3').verbose()
 
 // open the database
-let db = new sqlite3.Database('patientrecord.db', (err) => {
+let db = new sqlite3.Database('patient.db', (err) => {
     if (err) {
       console.error(err.message);
       throw err
     }
-    console.log('Connected to the patientrecord database.');
+    console.log('Connected to the patient database.');
   });
 
 
   // create table 'book'
-  const sql='CREATE TABLE patientrecord(name text)';
+  const sql='CREATE TABLE patient(name text)';
   db.run(sql, (err) => {
     if (err) {
         // Table already created
@@ -22,10 +22,10 @@ let db = new sqlite3.Database('patientrecord.db', (err) => {
       // First time Table created, insert some rows
       console.log('First time Table created, creating some rows.');
       
-      var insert = 'INSERT INTO patientrecord(name) VALUES(?)';
-      //db.run(insert, ['"treatId": "LHM908, "treatCourseId:"40220", "type:"long term", "category": "Asthma", "name": "Josh Drakeson", "startDate": "28-03-2021" ']);
-      db.run(insert, ['JavaScript Programming']);
-      db.run(insert, ['React with Hooks']);
+      var insert = 'INSERT INTO patient(name) VALUES(?)';
+      db.run(insert, ['"treatId": "LHM908, "treatCourseId:"40220", "type:"long term", "category": "Asthma", "name": "Josh Drakeson", "startDate": "28-03-2021" ']);
+      //db.run(insert, ['JavaScript Programming']);
+      //db.run(insert, ['React with Hooks']);
      
     }
   });
